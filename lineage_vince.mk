1ef76c4bb9dc5512af36130b2d26109b206047ef
Anggit Febriantoro <anggit.febriantoro@gmail.com>
2020-08-29 01:10:55 +0700

    vince : Change to MIUIv9 7.1.2 fp

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -38,7 +38,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
+    PRIVATE_BUILD_DESC="vince-user 7.1.2 N2G47H V9.5.4.0.NEGMIFA release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V11.0.2.0.OEGMIXM:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/vince/vince:7.1.2/N2G47H/V9.5.4.0.NEGMIFA:user/release-keys"
